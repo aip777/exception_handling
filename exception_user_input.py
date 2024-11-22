1af4458452b8c8b05e7b6ca694af555e6e1b3bd8
Palash <ariful.islamk777@gmail.com>
2024-11-22 23:52:36 +0600

    User input exception handling

---
+++ exception_user_input.py
@@ -0,0 +1,18 @@
+
+class NotAllowPercentage(Exception):
+    pass
+
+def calculate_percentage():
+    try:
+        num = int(input("Enter your number: "))
+        percentage = int(input("Enter your percentage(Do not allow 1-5%): "))
+        # Do not allow 1-5%
+        if percentage<=5:
+            raise NotAllowPercentage("Do not allow 1-5 %")
+        return (percentage * 100) / num
+    except (ValueError, NotAllowPercentage) as exp:
+        print(exp)
+        calculate_percentage()
+
+result = calculate_percentage()
+print(result)
